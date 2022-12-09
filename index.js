29bf8ec34f95f1ba08fee1c94438409bfa57009c
tbv125 <tbv125>
2022-12-09 15:44:53 +0300

    Cringe Version

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import ytdl from 'ytdl-core';
 import path from 'path';
-import { getHrefs } from './static/parser.js';
 import bodyParser from 'body-parser'
 
 const urlEncodedParser = bodyParser.urlencoded({
@@ -22,35 +21,21 @@ app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
 
+
+let downloadedNames = []
+
 app.get('/download', (req, res) => {
-    var URL = req.query.URL;
-    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    let URL = req.query.URL;
+    let name = req.query.name.trim()    
+    res.header('Content-Disposition', `attachment; filename=${downloadedNames.length}.mp3"`);
+    downloadedNames.push(name);
+    // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
     ytdl(URL, {
         quality: "lowestaudio"
     }).pipe(res);
+    console.log(downloadedNames[downloadedNames.length - 1]);
 });
 
 app.get('/', (req, res) => {
     res.render('index', { title: "Main Page", active: "main" });
 })
-
-app.post('/parseLinks', urlEncodedParser, (req, res) => {
-    if (!req.body) return res.sendStatus(400)
-    console.log(req.body);
-    getHrefs(req.body.URL)
-})
-
-// document.querySelectorAll('ytd-playlist-video-renderer').forEach((item, index) => {
-//     let link = item.querySelector('#content #container ytd-thumbnail a').getAttribute('href')
-//     console.log(`${index}: ${link}`)
-//     })    
-
-// let allLinks = document.querySelectorAll('ytd-playlist-video-renderer')
-// allLinks.forEach((item, index) => {
-//   let link = item.querySelector('#content #container ytd-thumbnail a').getAttribute('href')
-//   console.log(`${index}: ${link}`)
-// })
-
-// console.log(`https://www.youtube.com${allLinks[0]}`)
-
-// window.location.href = `http://localhost:4000/download?URL=https://www.youtube.com${allLinks[0]}`;
---
+++ monkey.js
@@ -0,0 +1,28 @@
+let allLinks = [];
+const activeLink = document.createElement('a');
+activeLink.target = "_blank"
+// allLinks = []
+document.querySelectorAll('ytd-playlist-video-renderer').forEach((item, index) => {
+    let link = item.querySelector('#content #container ytd-thumbnail a').getAttribute('href')
+    let name = item.querySelector('#content #container #meta h3 a').innerHTML
+    allLinks.push({
+        "URL": link,
+        "name": name
+    })
+})
+
+console.log(allLinks);
+
+// allLinks.forEach((item, index) => {
+//     activeLink.href = `http://localhost:4000/download?URL=https://www.youtube.com${item.URL}&name=${item.name}`
+//     activeLink.click()  	
+// })
+
+activeLink.href = `http://localhost:4000/download?URL=https://www.youtube.com${allLinks[0].URL}&name=${allLinks[0].name}`
+activeLink.click()
+
+for (let i = 0; i < allLinks.length; i++) {
+    activeLink.href = `http://localhost:4000/download?URL=https://www.youtube.com${allLinks[i].URL}&name=${allLinks[i].name}`
+    activeLink.click()
+    if (i > 5) break
+}
