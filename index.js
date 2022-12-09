c44029e015787acb4a832813f82272c5ec5af842
tbv125 <tbv125>
2022-12-09 12:41:49 +0300

    Остатки кода с попытками парсить внутри сервера

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import ytdl from 'ytdl-core';
 import path from 'path';
+import { getHrefs } from './static/parser.js';
+import bodyParser from 'body-parser'
+
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: false,
+})
 
 const app = express();
 const __dirname = path.resolve();
@@ -17,19 +23,34 @@ app.listen(4000, () => {
 });
 
 app.get('/download', (req, res) => {
-    var URL = req.query.URL;    
+    var URL = req.query.URL;
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
     ytdl(URL, {
         quality: "lowestaudio"
     }).pipe(res);
 });
 
-app.get('/', (req, res) => {        
-    res.render('index', { title: "Main Page", active: "main" });        
+app.get('/', (req, res) => {
+    res.render('index', { title: "Main Page", active: "main" });
 })
 
+app.post('/parseLinks', urlEncodedParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+    console.log(req.body);
+    getHrefs(req.body.URL)
+})
 
 // document.querySelectorAll('ytd-playlist-video-renderer').forEach((item, index) => {
 //     let link = item.querySelector('#content #container ytd-thumbnail a').getAttribute('href')
-//     console.log(`${index}: ${link}`)      
+//     console.log(`${index}: ${link}`)
 //     })    
+
+// let allLinks = document.querySelectorAll('ytd-playlist-video-renderer')
+// allLinks.forEach((item, index) => {
+//   let link = item.querySelector('#content #container ytd-thumbnail a').getAttribute('href')
+//   console.log(`${index}: ${link}`)
+// })
+
+// console.log(`https://www.youtube.com${allLinks[0]}`)
+
+// window.location.href = `http://localhost:4000/download?URL=https://www.youtube.com${allLinks[0]}`;
--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 var convertBtn = document.querySelector('.convert-button');
+var parseBtn = document.querySelector('.parse-button');
 var URLinput = document.querySelector('.URL-input');
 
 convertBtn.addEventListener('click', () => {
@@ -6,6 +7,24 @@ convertBtn.addEventListener('click', () => {
     sendURL(URLinput.value);
 });
 
+parseBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    parseLinks(URLinput.value);
+});
+
+function parseLinks(URL) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/parseLinks", true)
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(`URL=${URL}`)
+    xhr.onreadystatechange = () => {
+        console.log("Ура, ответ!!!");
+        console.log(xhr.readyState);
+        console.log('status:', xhr.status);        
+    }
+}
+
 function sendURL(URL) {
     window.location.href = `http://localhost:4000/download?URL=${URL}`;
 }
+
